c776d2e61e00db5bb8a27156e54ad9dd874f2c68
Shaskels <allasidner028@gmail.com>
2024-12-06 13:00:02 +0700

    Lab7

---
+++ Lab7/app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.lab7"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lab7"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-rxjava2:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("com.google.dagger:dagger:2.52")
+    kapt("com.google.dagger:dagger-compiler:2.52")
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ Lab7/app/src/main/java/com/example/lab7/data/datasource/ContactListLocalDataSource.kt
@@ -0,0 +1,11 @@
+package com.example.lab7.data.datasource
+
+import com.example.lab7.domain.presentation.ContactFromLocal
+import io.reactivex.Completable
+import io.reactivex.Single
+
+interface ContactListLocalDataSource {
+    fun getAllContacts(): Single<List<ContactFromLocal>>
+    fun appendContact(contact: ContactFromLocal): Completable
+    fun updateContact(contact: ContactFromLocal): Completable
+}
